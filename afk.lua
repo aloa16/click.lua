6c7dc783a3f28773db59bec17d746caa30594461
aloa16 <muratfattahov6@gmail.com>
2026-06-17 13:25:57 +0500

    Update afk.lua

--- afk.lua
+++ afk.lua
@@ -2,22 +2,19 @@ local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
--- Твой AccessCode из ссылки
 local ACCESS_CODE = "089c296b88655649ad9aae970ed7f46f" 
 
 local function rejoinPrivateServer()
     warn("Реконнект на приватку...")
     task.wait(2)
-    -- Телепортация на приватный сервер по коду
+    -- Здесь должны быть {LocalPlayer}
     TeleportService:TeleportToPrivateServer(game.PlaceId, ACCESS_CODE, {LocalPlayer})
 end
 
--- Мониторинг отключения
 game:GetService("CoreGui").RobloxGui.Modules.Common.Connection.ConnectionLost:Connect(function()
     rejoinPrivateServer()
 end)
 
--- Дополнительная страховка: проверка наличия игрока в DataModel
 task.spawn(function()
     while task.wait(10) do
         if not LocalPlayer or LocalPlayer.Parent == nil then
